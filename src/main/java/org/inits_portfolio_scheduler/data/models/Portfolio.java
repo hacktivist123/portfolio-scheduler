2fd91930a3cdb990cf68451693575eeb244d1381
olamideakintayo <divineolamide977@gmail.com>
2025-11-04 05:24:59 +0100

    Added Portfolio Model

---
+++ src/main/java/org/inits_portfolio_scheduler/data/models/Portfolio.java
@@ -0,0 +1,24 @@
+package org.inits_portfolio_scheduler.data.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Portfolio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String ownerEmail;
+    private String portfolioName;
+    private BigDecimal totalValue;
+    private BigDecimal interestRate;
+    private LocalDateTime lastUpdated;
+}
