312c249c98ee47a4f520897124e54a3d16473006
olamideakintayo <divineolamide977@gmail.com>
2025-11-04 05:04:46 +0100

    Initial commit

---
+++ src/main/java/org/inits_portfolio_scheduler/InitsPortfolioSchedulerApplication.java
@@ -0,0 +1,13 @@
+package org.inits_portfolio_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitsPortfolioSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitsPortfolioSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/inits_portfolio_scheduler/InitsPortfolioSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inits_portfolio_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitsPortfolioSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
