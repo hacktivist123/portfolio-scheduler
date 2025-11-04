efd2d4c82db90ebb7fa86ba4ea4b2deafb54c9e0
olamideakintayo <divineolamide977@gmail.com>
2025-11-04 08:08:47 +0100

    Added Global Exception Handler

---
+++ src/main/java/org/inits_portfolio_scheduler/dtos/responses/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package org.inits_portfolio_scheduler.dtos.responses;
+
+import org.inits_portfolio_scheduler.exceptions.PortfolioNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PortfolioNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handlePortfolioNotFound(PortfolioNotFoundException ex, WebRequest request) {
+        ApiErrorResponse response = new ApiErrorResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleGeneralException(Exception ex, WebRequest request) {
+        ApiErrorResponse response = new ApiErrorResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
