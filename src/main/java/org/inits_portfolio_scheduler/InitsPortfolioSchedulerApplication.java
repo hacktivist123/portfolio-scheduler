8ab8d56d1dffbfe265df75286ffefe81661ea274
olamideakintayo <divineolamide977@gmail.com>
2025-11-04 08:01:19 +0100

    Added Scheduler service and Annotate it at the main application

--- src/main/java/org/inits_portfolio_scheduler/InitsPortfolioSchedulerApplication.java
+++ src/main/java/org/inits_portfolio_scheduler/InitsPortfolioSchedulerApplication.java
@@ -2,8 +2,10 @@ package org.inits_portfolio_scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class InitsPortfolioSchedulerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/org/inits_portfolio_scheduler/scheduler/PortfolioInterestScheduler.java
@@ -0,0 +1,21 @@
+package org.inits_portfolio_scheduler.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.inits_portfolio_scheduler.services.PortfolioService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PortfolioInterestScheduler {
+
+    private final PortfolioService portfolioService;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void runDailyInterestUpdate() {
+        log.info("Running daily portfolio interest update...");
+        portfolioService.updateDailyInterest();
+    }
+}
